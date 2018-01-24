02cc59e065b48ffe22776aade7c99807d1a5cb10
ArturN <nizamutdin.art@yandex.ru>
2018-01-24 06:48:08 +0400

    Update build

--- preact.config.js
+++ preact.config.js
@@ -4,9 +4,13 @@ export default (config, env, helpers) => {
     const { rule } = helpers.getLoadersByName(config, 'babel-loader')[0];
     const babelConfig = rule.options;
 
-    babelConfig.plugins.push('lodash');
+    babelConfig.plugins.push('lodash', 'transform-async-to-promises');
+
+    const { plugin } = helpers.getPluginsByName(config, 'UglifyJsPlugin')[0];
+    plugin.options.sourceMap = false;
 
     if (env.production) {
+        config.output.publicPath = '/notes/';
         config.plugins.push(
             new BundleAnalyzerPlugin({
                 analyzerMode: 'static',
