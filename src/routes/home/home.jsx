03d535f7a86432aae5bc7f758fc8f781671abdf1
ArturJS <nizamutdin.art@gmail.com>
2018-02-02 19:47:43 +0400

    Started integration of the drag & drop functionality with firebase

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -29,21 +29,115 @@ export default class Home extends Component {
 
     getNotesRef = () => firebaseProvider.getCurrentUserData().child('notes');
 
+    getNoteRef = id =>
+        firebaseProvider.getCurrentUserData().child(`notes/${id}`);
+
+    mapNotes = initialNotesMap => {
+        // Important! Notes order is reversed!
+        if (_.isEmpty(initialNotesMap)) {
+            return [];
+        }
+
+        const notesMap = _.mapValues(initialNotesMap, (note, id) => ({
+            id,
+            title: note.title,
+            description: note.description,
+            files: note.files,
+            prev: note.prev,
+            next: note.next
+        }));
+        const notes = _.values(notesMap);
+        const firstNote = _.find(notes, note => !note.next);
+        const lastNote = _.find(notes, note => !note.prev);
+
+        if (!firstNote || !lastNote) {
+            console.error('Invalid notes data in Database!');
+            console.error(
+                `firstNote: ${JSON.stringify(
+                    firstNote
+                )}, lastNote: ${JSON.stringify(firstNote)}`
+            );
+
+            return [];
+        }
+
+        const result = [firstNote];
+        const passedNotesSet = new Set(result);
+        let nextNote = notesMap[firstNote.prev];
+
+        while (nextNote) {
+            // TODO: check possible circular links! (in case of inconsistent data in database)
+            if (passedNotesSet.has(nextNote)) {
+                console.error('Circular links in database!');
+                console.error(`note: ${JSON.stringify(nextNote)}`);
+
+                return [];
+            }
+            passedNotesSet.add(nextNote);
+            result.push(nextNote);
+            nextNote = notesMap[nextNote.prev];
+        }
+
+        return result;
+    };
+
     getNotes = () => {
         this.getNotesRef().on('value', snapshot => {
             const notesMap = snapshot.val();
-            const notes = _.entries(notesMap)
-                .map(([id, note]) => ({
-                    id,
-                    title: note.title,
-                    description: note.description,
-                    files: note.files,
-                    prev: note.prev,
-                    next: note.next
-                }))
-                .reverse();
+            const notes = this.mapNotes(notesMap);
+
             this.setState({ notes });
         });
+    };
+
+    connectSiblings = note => {
+        const { prev, next } = note;
+
+        if (prev) {
+            this.getNoteRef(prev).update({
+                next: next || null
+            });
+        }
+
+        if (next) {
+            this.getNoteRef(next).update({
+                prev: prev || null
+            });
+        }
+    };
+
+    insertBetweenNotes = (prevNextNotes, note) => {
+        const { prevNote, nextNote } = prevNextNotes;
+
+        if (prevNote) {
+            this.getNoteRef(prevNote.id).update({
+                next: note.id
+            });
+        }
+
+        if (nextNote) {
+            this.getNoteRef(nextNote.id).update({
+                prev: note.id
+            });
+        }
+
+        this.getNoteRef(note.id).update({
+            prev: prevNote ? prevNote.id : null,
+            next: nextNote ? nextNote.id : null
+        });
+    };
+
+    updateRelatedNotesRefs = (notes, currentNoteIndex) => {
+        const currentNote = notes[currentNoteIndex];
+
+        this.connectSiblings(currentNote);
+        this.insertBetweenNotes(
+            {
+                prevNote: notes[currentNoteIndex + 1],
+                nextNote: notes[currentNoteIndex - 1]
+            },
+            currentNote
+        );
     };
 
     onMoveNote = (dragIndex, hoverIndex) => {
@@ -55,7 +149,7 @@ export default class Home extends Component {
 
         // remove from dragIndex and insert into hoverIndex
         [[dragIndex, 1], [hoverIndex, 0, dragNote]].forEach(args => {
-            notes.splice.apply(notes, args); // eslint-disable-line
+            notes.splice(...args);
         });
 
         this.setState({
@@ -64,7 +158,9 @@ export default class Home extends Component {
     };
 
     onDropNote = (dragIndex, hoverIndex) => {
-        console.log('dragIndex ', dragIndex, ' hoverIndex ', hoverIndex);
+        const { notes } = this.state;
+
+        this.updateRelatedNotesRefs(notes, hoverIndex);
     };
 
     render() {
