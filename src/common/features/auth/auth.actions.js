ae75c704f473c9d8869d57b0b65d6c04cbe8272d
ArturN <nizamutdin.art@yandex.ru>
2018-04-14 16:45:29 +0400

    Fix auth.actions.js

--- src/common/features/auth/auth.actions.js
+++ src/common/features/auth/auth.actions.js
@@ -1,19 +1,20 @@
+import _ from 'lodash';
 import { createActions } from 'redux-actions';
 
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const { loginRequest, loginSuccess, loginFailure } = createActions({
-    [LOGIN_REQUEST]: () => {},
+    [LOGIN_REQUEST]: _.noop,
     [LOGIN_SUCCESS]: ({ email }) => ({ email }),
-    [LOGIN_FAILURE]: error => error
+    [LOGIN_FAILURE]: _.noop
 });
 
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
 export const { logoutRequest, logoutSuccess, logoutFailure } = createActions({
-    [LOGOUT_REQUEST]: () => {},
-    [LOGOUT_SUCCESS]: () => {},
-    [LOGOUT_FAILURE]: error => error
+    [LOGOUT_REQUEST]: _.noop,
+    [LOGOUT_SUCCESS]: _.noop,
+    [LOGOUT_FAILURE]: _.noop
 });
