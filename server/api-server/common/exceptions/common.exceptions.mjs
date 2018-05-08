9722e64fda57e4b1bc21e4281fc495f16c7fd517
ArturN <nizamutdin.art@yandex.ru>
2018-05-08 08:40:10 +0400

    Added 'errorCode' to exceptions

--- server/api-server/common/exceptions/common.exceptions.mjs
+++ server/api-server/common/exceptions/common.exceptions.mjs
@@ -1,7 +1,7 @@
 import HttpStatus from 'http-status-codes';
 
 export class Exception extends Error {
-    constructor(statusCode, message) {
+    constructor({ statusCode, errorCode, message }) {
         const errorMessage = Array.isArray(message)
             ? message.join('')
             : message;
@@ -9,15 +9,21 @@ export class Exception extends Error {
         super(errorMessage);
 
         this._statusCode = statusCode;
+        this._errorCode = errorCode;
     }
 
     get statusCode() {
         return this._statusCode;
     }
 
+    get errorCode() {
+        return this._errorCode;
+    }
+
     toObject() {
         return {
             statusCode: this._statusCode,
+            errorCode: this._errorCode,
             message: this.message
         };
     }
@@ -25,30 +31,50 @@ export class Exception extends Error {
 
 export class ErrorNotFound extends Exception {
     constructor(message) {
-        super(HttpStatus.NOT_FOUND, message);
+        super({
+            statusCode: HttpStatus.NOT_FOUND,
+            errorCode: 'ErrorNotFound',
+            message
+        });
     }
 }
 
 export class ErrorAlreadyExists extends Exception {
     constructor(message) {
-        super(HttpStatus.CONFLICT, message);
+        super({
+            statusCode: HttpStatus.CONFLICT,
+            errorCode: 'ErrorAlreadyExists',
+            message
+        });
     }
 }
 
 export class ErrorAccessDenied extends Exception {
     constructor(message) {
-        super(HttpStatus.FORBIDDEN, message);
+        super({
+            statusCode: HttpStatus.FORBIDDEN,
+            errorCode: 'ErrorAccessDenied',
+            message
+        });
     }
 }
 
 export class ErrorNotAuthorized extends Exception {
     constructor() {
-        super(HttpStatus.UNAUTHORIZED, 'Unauthorized!');
+        super({
+            statusCode: HttpStatus.UNAUTHORIZED,
+            errorCode: 'ErrorNotAuthorized',
+            message: 'Unauthorized!'
+        });
     }
 }
 
 export class ErrorBadRequest extends Exception {
     constructor(message) {
-        super(HttpStatus.BAD_REQUEST, message);
+        super({
+            statusCode: HttpStatus.BAD_REQUEST,
+            errorCode: 'ErrorBadRequest',
+            message
+        });
     }
 }
