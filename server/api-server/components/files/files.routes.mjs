4040935bdc960028abb1ef7f6d01cfb216c8485f
ArturN <nizamutdin.art@yandex.ru>
2018-05-09 19:11:50 +0400

    Add api validation for /files

--- server/api-server/components/files/files.routes.mjs
+++ server/api-server/components/files/files.routes.mjs
@@ -1,13 +1,24 @@
 import Router from 'koa-router';
 import { withAuth } from '../../common/middlewares/auth.middleware';
+import filesApiValidator from './files.api-validator';
 import filesController from './files.controller';
 
 const router = new Router();
 
 router
     .get('/files', withAuth, filesController.getAll)
-    .get('/files/:id', withAuth, filesController.getById)
+    .get(
+        '/files/:id',
+        withAuth,
+        filesApiValidator.getById,
+        filesController.getById
+    )
     .post('/files', withAuth, filesController.create)
-    .delete('/files/:id', withAuth, filesController.remove);
+    .delete(
+        '/files/:id',
+        withAuth,
+        filesApiValidator.remove,
+        filesController.remove
+    );
 
 export default router;
