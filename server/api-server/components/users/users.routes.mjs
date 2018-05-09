ed3c86485434abef7a846a1c56423357f0276ed9
ArturN <nizamutdin.art@yandex.ru>
2018-05-09 19:06:05 +0400

    Add api validation for /users

--- server/api-server/components/users/users.routes.mjs
+++ server/api-server/components/users/users.routes.mjs
@@ -1,13 +1,19 @@
 import Router from 'koa-router';
 import { withAuth } from '../../common/middlewares';
+import usersApiValidator from './users.api-validator';
 import usersController from './users.controller';
 
 const router = new Router();
 
 router
     .get('/users', withAuth, usersController.getAll)
-    .get('/users/:email', withAuth, usersController.getByEmail)
-    .post('/users', withAuth, usersController.create)
+    .get(
+        '/users/:email',
+        withAuth,
+        usersApiValidator.getByEmail,
+        usersController.getByEmail
+    )
+    .post('/users', withAuth, usersApiValidator.create, usersController.create)
     .get('/user', withAuth, usersController.getUserData);
 
 export default router;
