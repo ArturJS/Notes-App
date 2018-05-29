df7b99046c349de0f5b65aeb311f5bb17ee50ccd
ArturN <nizamutdin.art@yandex.ru>
2018-05-29 21:03:44 +0400

    Refactor <ModalDialog/> with using 'recompose'

--- ui-service/src/common/features/modal/modal-dialog.jsx
+++ ui-service/src/common/features/modal/modal-dialog.jsx
@@ -1,8 +1,9 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Modal from 'react-modal';
+import { compose, pure, withHandlers } from 'recompose';
 import _ from 'lodash';
 import * as modalActions from './modal.actions';
 import { MODAL_TYPES } from './modal.provider';
@@ -24,127 +25,133 @@ const mapDispatchToProps = dispatch => ({
     modalActions: bindActionCreators(modalActions, dispatch)
 });
 
-class ModalDialog extends Component {
-    static propTypes = {
-        modalStack: PropTypes.arrayOf(
-            PropTypes.shape({
-                id: PropTypes.string.isRequired,
-                title: PropTypes.string.isRequired,
-                body: PropTypes.oneOfType([
-                    PropTypes.string.isRequired,
-                    PropTypes.arrayOf(PropTypes.string.isRequired).isRequired
-                ]).isRequired,
-                type: PropTypes.string.isRequired,
-                close: PropTypes.func.isRequired,
-                className: PropTypes.string.isRequired,
-                shouldCloseOnOverlayClick: PropTypes.bool.isRequired,
-                noBackdrop: PropTypes.bool.isRequired
-            }).isRequired
-        ).isRequired,
-        modalActions: PropTypes.shape({
-            closeModal: PropTypes.func.isRequired
-        }).isRequired
-    };
+const enhance = compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    withHandlers({
+        close: props => event => {
+            const { id } = event.target.dataset;
 
-    close = event => {
-        const { id } = event.target.dataset;
+            props.modalActions.closeModal({
+                id,
+                reason: true
+            });
+        },
 
-        this.props.modalActions.closeModal({
-            id,
-            reason: true
-        });
-    };
+        dismiss: props => event => {
+            const { id } = event.target.dataset;
 
-    dismiss = event => {
-        const { id } = event.target.dataset;
+            props.modalActions.closeModal({
+                id,
+                reason: false
+            });
+        },
+        renderModalBody: () => modal => {
+            const isArrayOfStrings = _.isArray(modal.body);
 
-        this.props.modalActions.closeModal({
-            id,
-            reason: false
-        });
-    };
-
-    renderModalBody = modal => {
-        const isArrayOfStrings = _.isArray(modal.body);
-
-        return (
-            <Fragment>
-                {isArrayOfStrings
-                    ? _.map(modal.body, item => <p key={item}>{item}</p>)
-                    : modal.body}
-            </Fragment>
-        );
-    };
-
-    renderCustomType = modal => (
-        <div className="modal-custom-body">{this.renderModalBody(modal)}</div>
-    );
-
-    renderStandardType = modal => (
-        <div>
-            <div className="modal-body">{this.renderModalBody(modal)}</div>
-            <div className="modal-footer">
-                <button
-                    className="btn btn-primary btn-ok"
-                    type="button"
-                    data-id={modal.id}
-                    onClick={this.close}
-                >
-                    Ok
-                </button>
-                {modal.type === MODAL_TYPES.confirm && (
+            return (
+                <Fragment>
+                    {isArrayOfStrings
+                        ? _.map(modal.body, item => <p key={item}>{item}</p>)
+                        : modal.body}
+                </Fragment>
+            );
+        }
+    }),
+    withHandlers({
+        renderCustomType: ({ renderModalBody }) => modal => (
+            <div className="modal-custom-body">{renderModalBody(modal)}</div>
+        ),
+        renderStandardType: ({ renderModalBody, close, dismiss }) => modal => (
+            <div>
+                <div className="modal-body">{renderModalBody(modal)}</div>
+                <div className="modal-footer">
                     <button
-                        className="btn btn-default btn-cancel"
+                        className="btn btn-primary btn-ok"
                         type="button"
                         data-id={modal.id}
-                        onClick={this.dismiss}
+                        onClick={close}
                     >
-                        Cancel
+                        Ok
                     </button>
-                )}
+                    {modal.type === MODAL_TYPES.confirm && (
+                        <button
+                            className="btn btn-default btn-cancel"
+                            type="button"
+                            data-id={modal.id}
+                            onClick={dismiss}
+                        >
+                            Cancel
+                        </button>
+                    )}
+                </div>
             </div>
-        </div>
-    );
+        )
+    }),
+    pure
+);
 
-    render() {
-        const { modalStack } = this.props;
-
-        return (
-            <Fragment>
-                {modalStack.map(modal => (
-                    <Modal
-                        key={modal.id}
-                        isOpen
-                        onRequestClose={this.dismiss}
-                        style={modal.noBackdrop ? noBackdropStyle : {}}
-                        className={`modal ${modal.className}`}
-                        shouldCloseOnOverlayClick={
-                            modal.shouldCloseOnOverlayClick
-                        }
-                        contentLabel=""
+const ModalDialog = ({
+    modalStack,
+    dismiss,
+    renderCustomType,
+    renderStandardType
+}) => (
+    <Fragment>
+        {modalStack.map(modal => (
+            <Modal
+                key={modal.id}
+                isOpen
+                onRequestClose={dismiss}
+                style={modal.noBackdrop ? noBackdropStyle : {}}
+                className={`modal ${modal.className}`}
+                shouldCloseOnOverlayClick={modal.shouldCloseOnOverlayClick}
+                contentLabel=""
+            >
+                <div className="modal-content">
+                    <button
+                        type="button"
+                        className="close"
+                        data-id={modal.id}
+                        onClick={dismiss}
                     >
-                        <div className="modal-content">
-                            <button
-                                type="button"
-                                className="close"
-                                data-id={modal.id}
-                                onClick={this.dismiss}
-                            >
-                                &times;
-                            </button>
-                            <div className="modal-header">
-                                <h3 className="modal-title">{modal.title}</h3>
-                            </div>
-                            {modal.type === MODAL_TYPES.custom &&
-                                this.renderCustomType(modal)}
-                            {modal.type !== MODAL_TYPES.custom &&
-                                this.renderStandardType(modal)}
-                        </div>
-                    </Modal>
-                ))}
-            </Fragment>
-        );
-    }
-}
+                        &times;
+                    </button>
+                    <div className="modal-header">
+                        <h3 className="modal-title">{modal.title}</h3>
+                    </div>
+                    {modal.type === MODAL_TYPES.custom &&
+                        renderCustomType(modal)}
+                    {modal.type !== MODAL_TYPES.custom &&
+                        renderStandardType(modal)}
+                </div>
+            </Modal>
+        ))}
+    </Fragment>
+);
 
-export default connect(mapStateToProps, mapDispatchToProps)(ModalDialog);
+ModalDialog.propTypes = {
+    modalStack: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            title: PropTypes.string.isRequired,
+            body: PropTypes.oneOfType([
+                PropTypes.string.isRequired,
+                PropTypes.arrayOf(PropTypes.string.isRequired).isRequired
+            ]).isRequired,
+            type: PropTypes.string.isRequired,
+            close: PropTypes.func.isRequired,
+            className: PropTypes.string.isRequired,
+            shouldCloseOnOverlayClick: PropTypes.bool.isRequired,
+            noBackdrop: PropTypes.bool.isRequired
+        }).isRequired
+    ).isRequired,
+    // eslint-disable-next-line react/no-unused-prop-types
+    modalActions: PropTypes.shape({
+        closeModal: PropTypes.func.isRequired
+    }).isRequired,
+    dismiss: PropTypes.func.isRequired,
+    renderCustomType: PropTypes.func.isRequired,
+    renderStandardType: PropTypes.func.isRequired
+};
+
+export default enhance(ModalDialog);
