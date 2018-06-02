c9e6a582011272b088f9f6d1912085b67e6da7cf
ArturN <nizamutdin.art@yandex.ru>
2018-06-02 21:54:56 +0400

    Fixed wrong redirect after /sign-in

--- ui-service/src/common/api/auth.api.js
+++ ui-service/src/common/api/auth.api.js
@@ -4,7 +4,8 @@ import { baseApi } from './base.api';
 export const authApi = {
     loginWithGoogle() {
         const { baseURL } = config;
-        const returnUrl = encodeURIComponent(window.location.pathname);
+        const { origin, pathname } = window.location;
+        const returnUrl = encodeURIComponent(`${origin}${pathname}`);
 
         window.location.replace(
             `${baseURL}/auth/google?returnUrl=${returnUrl}`
