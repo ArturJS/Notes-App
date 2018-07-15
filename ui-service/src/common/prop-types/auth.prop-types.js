f53bfeed0c984c4f894d87891b4e8a66c0eb24ad
ArturJS <nizamutdin.art@gmail.com>
2018-07-15 11:23:56 +0400

    Added unit test for <UserAuthState/> (also fixed wrong authPropTypes and updated 'husky')

--- ui-service/src/common/prop-types/auth.prop-types.js
+++ ui-service/src/common/prop-types/auth.prop-types.js
@@ -2,9 +2,5 @@ import PropTypes from 'prop-types';
 
 export const authActionsPropType = PropTypes.shape({
     loginRequest: PropTypes.func.isRequired,
-    loginSuccess: PropTypes.func.isRequired,
-    loginFailure: PropTypes.func.isRequired,
-    logoutRequest: PropTypes.func.isRequired,
-    logoutSuccess: PropTypes.func.isRequired,
-    logoutFailure: PropTypes.func.isRequired
+    logoutRequest: PropTypes.func.isRequired
 });
