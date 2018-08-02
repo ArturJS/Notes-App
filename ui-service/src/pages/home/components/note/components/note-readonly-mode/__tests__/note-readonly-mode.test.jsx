d4969841e0262ce595b1b5e4bf8b58d66405fb19
ArturJS <nizamutdin.art@gmail.com>
2018-08-02 08:23:41 +0400

    Added unit test for <NoteReadonlyMode/>

--- ui-service/src/pages/home/components/note/components/note-readonly-mode/__tests__/note-readonly-mode.test.jsx
+++ ui-service/src/pages/home/components/note/components/note-readonly-mode/__tests__/note-readonly-mode.test.jsx
@@ -8,10 +8,14 @@ jest.mock('../../../../file-list', () => props => (
 ));
 
 /* eslint-disable import/first */
+import { mount } from 'enzyme';
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { modalProvider } from '@common/features/modal';
 import NoteReadonlyMode from '../note-readonly-mode';
 /* eslint-enable import/first */
+
+const flushPromises = () => new Promise(resolve => setImmediate(resolve));
 
 describe('<NoteReadonlyMode />', () => {
     it('renders correctly', () => {
@@ -26,7 +30,7 @@ describe('<NoteReadonlyMode />', () => {
                     filename: 'file name'
                 }
             ],
-            trackId: 1,
+            trackId: '1',
             meta: {
                 transactionId: 1
             }
@@ -42,5 +46,410 @@ describe('<NoteReadonlyMode />', () => {
             .toJSON();
 
         expect(tree).toMatchSnapshot();
+    });
+
+    describe('test isLoading state', () => {
+        it('should have `isLoading` class when note has `meta.transactionId`', () => {
+            const note = {
+                id: 1,
+                title: 'Note test title',
+                description: 'Note test description',
+                files: [
+                    {
+                        id: 12,
+                        downloadPath: '/test/download/path',
+                        filename: 'file name'
+                    }
+                ],
+                trackId: '1',
+                meta: {
+                    transactionId: 1
+                }
+            };
+            const wrapper = mount(
+                <NoteReadonlyMode
+                    note={note}
+                    onRemove={() => {}}
+                    onEdit={() => {}}
+                />
+            );
+
+            expect(wrapper.render().hasClass('isLoading')).toBe(true);
+        });
+
+        it('should NOT have `isLoading` class when note do NOT have `meta.transactionId`', () => {
+            const note = {
+                id: 1,
+                title: 'Note test title',
+                description: 'Note test description',
+                files: [
+                    {
+                        id: 12,
+                        downloadPath: '/test/download/path',
+                        filename: 'file name'
+                    }
+                ],
+                trackId: '1'
+            };
+            const wrapper = mount(
+                <NoteReadonlyMode
+                    note={note}
+                    onRemove={() => {}}
+                    onEdit={() => {}}
+                />
+            );
+
+            expect(wrapper.render().hasClass('isLoading')).toBe(false);
+        });
+    });
+
+    describe('`onEdit` prop', () => {
+        describe('it should be called after ', () => {
+            it('clicked by .icon-left', () => {
+                const note = {
+                    id: 1,
+                    title: 'Note test title',
+                    description: 'Note test description',
+                    files: [
+                        {
+                            id: 12,
+                            downloadPath: '/test/download/path',
+                            filename: 'file name'
+                        }
+                    ],
+                    trackId: '1'
+                };
+                const onEdit = jest.fn();
+                const wrapper = mount(
+                    <NoteReadonlyMode
+                        note={note}
+                        onRemove={() => {}}
+                        onEdit={onEdit}
+                    />
+                );
+
+                wrapper.find('.icon-left').simulate('click');
+
+                expect(onEdit).toHaveBeenCalledWith();
+            });
+
+            it('keyPressed by .icon-left', () => {
+                const note = {
+                    id: 1,
+                    title: 'Note test title',
+                    description: 'Note test description',
+                    files: [
+                        {
+                            id: 12,
+                            downloadPath: '/test/download/path',
+                            filename: 'file name'
+                        }
+                    ],
+                    trackId: '1'
+                };
+                const onEdit = jest.fn();
+                const wrapper = mount(
+                    <NoteReadonlyMode
+                        note={note}
+                        onRemove={() => {}}
+                        onEdit={onEdit}
+                    />
+                );
+
+                wrapper.find('.icon-left').simulate('keyPress');
+
+                expect(onEdit).toHaveBeenCalledWith();
+            });
+        });
+
+        describe('it should NOT be called (if isLoading) after ', () => {
+            it('clicked by .icon-left', () => {
+                const note = {
+                    id: 1,
+                    title: 'Note test title',
+                    description: 'Note test description',
+                    files: [
+                        {
+                            id: 12,
+                            downloadPath: '/test/download/path',
+                            filename: 'file name'
+                        }
+                    ],
+                    trackId: '1',
+                    meta: {
+                        transactionId: 1
+                    }
+                };
+                const onEdit = jest.fn();
+                const wrapper = mount(
+                    <NoteReadonlyMode
+                        note={note}
+                        onRemove={() => {}}
+                        onEdit={onEdit}
+                    />
+                );
+
+                wrapper.find('.icon-left').simulate('click');
+
+                expect(onEdit).not.toHaveBeenCalledWith();
+            });
+
+            it('keyPressed by .icon-left', () => {
+                const note = {
+                    id: 1,
+                    title: 'Note test title',
+                    description: 'Note test description',
+                    files: [
+                        {
+                            id: 12,
+                            downloadPath: '/test/download/path',
+                            filename: 'file name'
+                        }
+                    ],
+                    trackId: '1',
+                    meta: {
+                        transactionId: 1
+                    }
+                };
+                const onEdit = jest.fn();
+                const wrapper = mount(
+                    <NoteReadonlyMode
+                        note={note}
+                        onRemove={() => {}}
+                        onEdit={onEdit}
+                    />
+                );
+
+                wrapper.find('.icon-left').simulate('keyPress');
+
+                expect(onEdit).not.toHaveBeenCalledWith();
+            });
+        });
+    });
+
+    describe('`onRemove` prop', () => {
+        describe('it should be called after ', () => {
+            it('clicked by .icon-right and modalProvider.showConfirm(...) confirms action', async () => {
+                const note = {
+                    id: 132,
+                    title: 'Note test title',
+                    description: 'Note test description',
+                    files: [
+                        {
+                            id: 12,
+                            downloadPath: '/test/download/path',
+                            filename: 'file name'
+                        }
+                    ],
+                    trackId: '1'
+                };
+                const onRemove = jest.fn();
+
+                modalProvider.showConfirm.mockImplementation(() => ({
+                    result: Promise.resolve(true)
+                }));
+
+                const wrapper = mount(
+                    <NoteReadonlyMode
+                        note={note}
+                        onRemove={onRemove}
+                        onEdit={() => {}}
+                    />
+                );
+
+                wrapper.find('.icon-right').simulate('click');
+
+                await flushPromises();
+
+                expect(onRemove).toHaveBeenCalledWith(note.id);
+            });
+
+            it('keyPressed by .icon-right and modalProvider.showConfirm(...) confirms action', async () => {
+                const note = {
+                    id: 132,
+                    title: 'Note test title',
+                    description: 'Note test description',
+                    files: [
+                        {
+                            id: 12,
+                            downloadPath: '/test/download/path',
+                            filename: 'file name'
+                        }
+                    ],
+                    trackId: '1'
+                };
+                const onRemove = jest.fn();
+
+                modalProvider.showConfirm.mockImplementation(() => ({
+                    result: Promise.resolve(true)
+                }));
+
+                const wrapper = mount(
+                    <NoteReadonlyMode
+                        note={note}
+                        onRemove={onRemove}
+                        onEdit={() => {}}
+                    />
+                );
+
+                wrapper.find('.icon-right').simulate('keyPress');
+
+                await flushPromises();
+
+                expect(onRemove).toHaveBeenCalledWith(note.id);
+            });
+        });
+
+        describe('it should NOT be called after ', () => {
+            describe('when isLoading and after ', () => {
+                it('clicked by .icon-right and modalProvider.showConfirm(...) confirms action', async () => {
+                    const note = {
+                        id: 132,
+                        title: 'Note test title',
+                        description: 'Note test description',
+                        files: [
+                            {
+                                id: 12,
+                                downloadPath: '/test/download/path',
+                                filename: 'file name'
+                            }
+                        ],
+                        trackId: '1',
+                        meta: {
+                            transactionId: 1
+                        }
+                    };
+                    const onRemove = jest.fn();
+
+                    modalProvider.showConfirm.mockImplementation(() => ({
+                        result: Promise.resolve(true)
+                    }));
+
+                    const wrapper = mount(
+                        <NoteReadonlyMode
+                            note={note}
+                            onRemove={onRemove}
+                            onEdit={() => {}}
+                        />
+                    );
+
+                    wrapper.find('.icon-right').simulate('click');
+
+                    await flushPromises();
+
+                    expect(onRemove).not.toHaveBeenCalledWith(note.id);
+                });
+
+                it('keyPressed by .icon-right and modalProvider.showConfirm(...) confirms action', async () => {
+                    const note = {
+                        id: 132,
+                        title: 'Note test title',
+                        description: 'Note test description',
+                        files: [
+                            {
+                                id: 12,
+                                downloadPath: '/test/download/path',
+                                filename: 'file name'
+                            }
+                        ],
+                        trackId: '1',
+                        meta: {
+                            transactionId: 1
+                        }
+                    };
+                    const onRemove = jest.fn();
+
+                    modalProvider.showConfirm.mockImplementation(() => ({
+                        result: Promise.resolve(true)
+                    }));
+
+                    const wrapper = mount(
+                        <NoteReadonlyMode
+                            note={note}
+                            onRemove={onRemove}
+                            onEdit={() => {}}
+                        />
+                    );
+
+                    wrapper.find('.icon-right').simulate('keyPress');
+
+                    await flushPromises();
+
+                    expect(onRemove).not.toHaveBeenCalledWith(note.id);
+                });
+            });
+
+            describe('when NOT isLoading and after ', () => {
+                it('clicked by .icon-right and modalProvider.showConfirm(...) NOT confirms action', async () => {
+                    const note = {
+                        id: 132,
+                        title: 'Note test title',
+                        description: 'Note test description',
+                        files: [
+                            {
+                                id: 12,
+                                downloadPath: '/test/download/path',
+                                filename: 'file name'
+                            }
+                        ],
+                        trackId: '1'
+                    };
+                    const onRemove = jest.fn();
+
+                    modalProvider.showConfirm.mockImplementation(() => ({
+                        result: Promise.resolve(false)
+                    }));
+
+                    const wrapper = mount(
+                        <NoteReadonlyMode
+                            note={note}
+                            onRemove={onRemove}
+                            onEdit={() => {}}
+                        />
+                    );
+
+                    wrapper.find('.icon-right').simulate('click');
+
+                    await flushPromises();
+
+                    expect(onRemove).not.toHaveBeenCalled();
+                });
+
+                it('keyPressed by .icon-right and modalProvider.showConfirm(...) NOT confirms action', async () => {
+                    const note = {
+                        id: 132,
+                        title: 'Note test title',
+                        description: 'Note test description',
+                        files: [
+                            {
+                                id: 12,
+                                downloadPath: '/test/download/path',
+                                filename: 'file name'
+                            }
+                        ],
+                        trackId: '1'
+                    };
+                    const onRemove = jest.fn();
+
+                    modalProvider.showConfirm.mockImplementation(() => ({
+                        result: Promise.resolve(false)
+                    }));
+
+                    const wrapper = mount(
+                        <NoteReadonlyMode
+                            note={note}
+                            onRemove={onRemove}
+                            onEdit={() => {}}
+                        />
+                    );
+
+                    wrapper.find('.icon-right').simulate('keyPress');
+
+                    await flushPromises();
+
+                    expect(onRemove).not.toHaveBeenCalled();
+                });
+            });
+        });
     });
 });
