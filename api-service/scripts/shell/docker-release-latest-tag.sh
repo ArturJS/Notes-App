aa829d1b65e79bc709a7390053d94a504cd13016
ArturJS <nizamutdin.art@gmail.com>
2019-01-03 21:46:28 +0400

    Use docker-release-tag.sh instead of docker-release-latest-tag.sh for api-service

--- api-service/scripts/shell/docker-release-latest-tag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker build -t notes-app_api-service:latest . &&
-docker tag notes-app_api-service:latest arturjs/notes-app_api-service:latest &&
-docker push arturjs/notes-app_api-service:latest
